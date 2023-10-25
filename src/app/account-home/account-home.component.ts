1be667f04ab118ab4d71a69569c594927f195e5d
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-25 09:12:53 -0400

    fetch recipe list data from the backend for account home page

--- src/app/account-home/account-home.component.ts
+++ src/app/account-home/account-home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-account-home',
   templateUrl: './account-home.component.html',
   styleUrls: ['./account-home.component.scss']
 })
-export class AccountHomeComponent {
+export class AccountHomeComponent implements OnInit {
+recipeList: any
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit():void {
+    this.recipeService.getAllRecipes().subscribe((response:any) => {
+      this.recipeList = response.data
+      //console.log(this.recipeList)
+    })
+  }
 }
