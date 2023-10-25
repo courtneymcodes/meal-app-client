deff2a37c772281305af196da49ccd724b8cfa37
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-25 12:42:38 -0400

    add log out functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  signOut() {
+    console.log(localStorage.getItem('jwt'))
+    //remove jwt token from local storage
+    localStorage.removeItem('jwt')
+    //navigate back to landing page
+    this.router.navigate(['/'])
+    
+    console.log(localStorage.getItem('jwt'))
+}
 
 }
