e55fbb9dfac4c34e502dfb87b08aef5de7d9c830
courtneymcodes <63630567+courtneymcodes@users.noreply.github.com>
2023-10-26 20:31:20 -0400

    add ability to save recipe from api to favorites list

--- src/app/generator/show-recipe/show-recipe.component.ts
+++ src/app/generator/show-recipe/show-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-show-recipe',
@@ -8,8 +9,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ShowRecipeComponent implements OnInit{
   @Input() recipe: any
 
+  constructor(private recipeService: RecipeService) {}
+
   ngOnInit() {
     console.log(this.recipe)
+  }
+
+  saveRecipe() {
+    this.recipeService.createRecipe(this.recipe).subscribe((response) => {
+      console.log(response)
+    })
   }
 
 }
